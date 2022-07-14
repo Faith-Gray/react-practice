f0f3db196b2c17b9597f7e66e845ca282178c687
Faith <fhgray96@gmail.com>
2022-07-14 18:59:15 -0500

    add component

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
 
 //render an <ul> with 2+ <li>s inside*/
 ReactDOM.render(<ul><li>Object 1</li> <li>Object2</li></ul>, document.getElementById("root"));
+
+
+function MainContent() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <Navbar />
+        <MainContent />
+    </div>,
+    document.getElementById("root")
+)
