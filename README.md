522415e3f988d83cd3a7a9128f70d88482b4f99d
Faith <fhgray96@gmail.com>
2022-07-14 18:39:00 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # react-practice
+This is react practice following the Scrimba course linked on TOP
---
+++ index.js
@@ -0,0 +1,4 @@
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+
+//render an <ul> with 2+ <li>s inside*/
+ReactDOM.render(<ul><li>Object 1</li> <li>Object2</li></ul>, document.getElementById("root"));
